96fe63ba2da15e96a0024abe2800f2184f3f62f2
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-06-29 01:56:05 +0900

    [ADD] crod 추가

--- src/main/java/org/example/hufshackaton/controller/CustomBotController.java
+++ src/main/java/org/example/hufshackaton/controller/CustomBotController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/bot")
+@CrossOrigin(origins = "*")
 public class CustomBotController {
 
     private final CustomBotService customBotService;
